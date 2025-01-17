e57dc4b1ab3618f1afd4bb770a0f87544cf53ec4
Michelle Kenia <michellekeniadev@gmail.com>
2025-01-16 21:04:30 -0300

    config-inicial

---
+++ src/main/java/br/com/michellek/quarkussocial/rest/dto/CreateUserRequest.java
@@ -0,0 +1,14 @@
+package br.com.michellek.quarkussocial.rest.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateUserRequest {
+
+    @NotBlank(message = "Name is required.")
+    private String name;
+    @NotNull(message = "Age is required.")
+    private Integer age;
+
+}
